dc6a2379d09e8a6cb43c03e5c9dd6e26d60ad108
Aatif Mastan <aatifmastan644@gmail.com>
2023-01-19 21:19:08 +0530

    fixed views +2

--- src/Pages/Bloginfo.jsx
+++ src/Pages/Bloginfo.jsx
@@ -3,16 +3,13 @@ import "./blog.css";
 import axios from "axios";
 // import { useNavigate } from "react-router-dom";
 
-
 export default function National() {
   const [blogdata, setBlogData] = React.useState([]);
-//   const navigate = useNavigate();
-  
+  //   const navigate = useNavigate();
 
   const getblog = () => {
-   
-   const id1=localStorage.getItem("id");
-   console.log(id1);
+    const id1 = localStorage.getItem("id");
+    console.log(id1);
     axios
       .get(`https://gleaming-hare-attire.cyclic.app/getposts/posts/${id1}`)
       .then((res) => {
@@ -25,26 +22,24 @@ export default function National() {
   };
   useEffect(() => {
     getblog();
-    
   }, []);
 
   return (
     <>
       <div className="main_list">
         <div className="logo">
-            <a href="https://clinqo.com">
-          <img
-            src="./assets/clinqologo.png"
-            alt="Error"
-          />
-            </a>
-        </div>  
+          <a href="https://clinqo.com">
+            <img src="./assets/clinqologo.png" alt="Error" />
+          </a>
+        </div>
 
         <div className="list">
-
-            <h1>{blogdata.title}</h1>
-            <p style={{color:"white",wordSpacing:"1rem",fontSize:"1.5rem"}}>{blogdata.content}</p>
-          
+          <h1>{blogdata.title}</h1>
+          <p
+            style={{ color: "white", wordSpacing: "1rem", fontSize: "1.5rem" }}
+          >
+            {blogdata.content}
+          </p>
         </div>
       </div>
     </>
