bcd121c0084b3a3494b11dd90664a13ee3c287a5
vratant11 <vratant2012061@akgec.ac.in>
2023-01-19 21:11:15 +0530

    Task done

--- src/Pages/WriteBlog.jsx
+++ src/Pages/WriteBlog.jsx
@@ -1,33 +1,45 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./writeBlog.css";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function National() {
-  const [blogdata, setBlogData] = React.useState([]);
-  const navigate = useNavigate();
-  const url = "https://gleaming-hare-attire.cyclic.app/getposts/all";
+  const [title, setTitle] = React.useState([]);
+  const [content, setContent] = React.useState([]);
+  const [author, setAuthor] = React.useState([]);
 
-  const getblog = () => {
-    axios
-      .get(url)
+//   const navigate = useNavigate();
+  const url = "https://gleaming-hare-attire.cyclic.app/addpost";
+
+  const Submit = () => {
+    const data = {
+        "title": title,
+        "content" : content,
+        "author" : author,
+        "views" : 0,
+      }
+      if(title===""||content===""||author===""){
+        alert("Fill all the fields");
+      }
+      else{
+        axios
+      .post(url,data)
       .then((res) => {
-        setBlogData(res.data);
-        // console.log(res.data);
+        // setBlogData(res.data);
+        console.log(res.data);
+        alert("Blog uploaded successfully");
       })
       .catch((err) => {
         console.log(err);
       });
+      }
+    
   };
 
-  const Submit = (key) => {
-    const ids = key;
-    localStorage.setItem("id", ids);
-    navigate("/bloginfo");
-  };
-  useEffect(() => {
-    getblog();
-  }, []);
+ 
+//   useEffect(() => {
+//     getblog();
+//   }, []);
 
   return (
     <>
@@ -42,7 +54,7 @@ export default function National() {
           <tr>
               <td>Title:</td>
               <td>
-                <input type="text"></input>
+                <input type="text" name="Heading" value={title} onChange={(e)=>{setTitle(e.target.value)}}></input>
                 {/* <textarea
                   id="w3review"
                   name="w3review"
@@ -57,16 +69,17 @@ export default function National() {
                 {/* <input type="text"></input> */}
                 <textarea
                   id="w3review"
-                  name="w3review"
+                  name="blog"
                   rows="4"
                   cols="50"
+                  value={content} onChange={(e)=>{setContent(e.target.value)}}
                 ></textarea>
               </td>
             </tr>
             <tr>
               <td>Author:</td>
               <td>
-                <input type="text"></input>
+                <input type="text" name="auth" value={author} onChange={(e)=>{setAuthor(e.target.value)}}></input>
                 {/* <textarea
                   id="w3review"
                   name="w3review"
@@ -75,12 +88,14 @@ export default function National() {
                 ></textarea> */}
               </td>
             </tr>
-            <div>
-                <button onClick={Submit}>Submit</button>
-            </div>
+           
             
           </table>
+         
         </div>
+        <div className="btn">
+                <button onClick={Submit}>Submit</button>
+            </div>
       </div>
     </>
   );
